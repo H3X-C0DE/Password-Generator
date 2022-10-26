e97302530ab3ab510095e9884e716370bb5af92c
IIHEXYII <vetletessem@gmail.com>
2022-10-26 13:41:38 +0200

    late commit

---
+++ assets/javascript.js
@@ -0,0 +1,123 @@
+const keys = {
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  number: "0123456789",
+  symbol: "!@#$%^&*()_+~`|}{[]:;?><,./-=",
+};
+const getKey = [
+  function upperCase() {
+    return keys.upperCase[Math.floor(Math.random() * keys.upperCase.length)];
+  },
+  function lowerCase() {
+    return keys.lowerCase[Math.floor(Math.random() * keys.lowerCase.length)];
+  },
+  function number() {
+    return keys.number[Math.floor(Math.random() * keys.number.length)];
+  },
+  function symbol() {
+    return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
+  },
+];
+
+function createPassword() {
+  const upper = document.getElementById("upperCase").checked;
+  const lower = document.getElementById("lowerCase").checked;
+  const number = document.getElementById("number").checked;
+  const symbol = document.getElementById("symbol").checked;
+  if (upper + lower + number + symbol === 0) {
+    alert("Please check atleast one box!");
+    return;
+  }
+  const passwordBox = document.getElementById("passwordBox");
+  const length = document.getElementById("length");
+  let password = "";
+  while (length.value > password.length) {
+    let keyToAdd = getKey[Math.floor(Math.random() * getKey.length)];
+    let isChecked = document.getElementById(keyToAdd.name).checked;
+    if (isChecked) {
+      password += keyToAdd();
+    }
+  }
+  passwordBox.innerHTML = password;
+}
+function copyPassword() {
+  const textarea = document.createElement("textarea");
+  const password = document.getElementById("passwordBox").innerText;
+  if (!password) {
+    return;
+  }
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard");
+}
+
+// decrypting
+const text = baffle(".data");
+text.set({
+  characters: "zpxzVpasdfh86136░█▒ ░░░█▓ >░░ ▓/▒█▓ █ █>█▒sayg▒ ░░░█▓ >yf",
+  speed: 150,
+});
+text.start();
+//revel speed
+text.reveal(6500);
+
+const text1 = baffle(".quote");
+text1.set({
+  characters: "qwertyuiopasdfgh8613611888",
+  speed: 140,
+});
+text1.start();
+//revel speed
+text1.reveal(6500);
+
+//-----------------BG ANIMATION-----------------------//
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+
+//making the canvas full screen
+c.height = window.innerHeight;
+c.width = window.innerWidth;
+
+var matrix =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+//converting the string into an array of single characters
+matrix = matrix.split("");
+
+var font_size = 10;
+//number of columns for the rain
+var columns = c.width / font_size;
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for (var x = 0; x < columns; x++) drops[x] = 1;
+
+//drawing the characters
+function draw() {
+  //Black BG for the canvas
+  //translucent BG to show trail
+  ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+  ctx.fillRect(0, 0, c.width, c.height);
+
+  ctx.fillStyle = "#667eea"; //green text
+  ctx.font = font_size + "px arial";
+  //looping over drops
+  for (var i = 0; i < drops.length; i++) {
+    //a random character to print
+    var text = matrix[Math.floor(Math.random() * matrix.length)];
+    //x = i*font_size, y = value of drops[i]*font_size
+    ctx.fillText(text, i * font_size, drops[i] * font_size);
+
+    //sending the drop back to the top randomly after it has crossed the screen
+    //adding a randomness to the reset to make the drops scattered on the Y axis
+    if (drops[i] * font_size > c.height && Math.random() > 0.975) drops[i] = 0;
+
+    //incrementing Y coordinate
+    drops[i]++;
+  }
+}
+
+setInterval(draw, 35);
